de521b44d261bdb35c9e320451109c2093b9d996
HouariZegai <houarizegai14@gmail.com>
2018-07-25 22:04:29 +0100

    adding list and adding form-login-model1

--- src/com/houarizegai/javafxtools/Launch.java
+++ src/com/houarizegai/javafxtools/Launch.java
@@ -5,14 +5,16 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Launch extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/javafxtools/box/model1/Model1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/javafxtools/lists/Lists.fxml"));
         
+        stage.setScene(new Scene(root));
         stage.setTitle("JavaFx Tools");
         stage.show();
     }
---
+++ src/com/houarizegai/javafxtools/forms/login/model1/LoginController.java
@@ -0,0 +1,6 @@
+package com.houarizegai.javafxtools.forms.login.model1;
+
+public class LoginController {
+
+
+}
