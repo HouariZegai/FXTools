a3b0de0a1d1f30a455450f96068e6536bcaa429e
houarizegai <houarizegai14@gmail.com>
2018-09-23 17:39:03 +0200

    Add Libs

--- src/com/houarizegai/javafxtools/Launch.java
+++ src/com/houarizegai/javafxtools/Launch.java
@@ -13,14 +13,10 @@ public class Launch extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/javafxtools/cards/Cards.fxml"));
-        
+
         stage.setScene(new Scene(root));
         stage.setTitle("JavaFx Tools");
         stage.show();
-    }
-    
-    public static void main(String[] args) {
-        launch(args);
     }
     
 }
