817e43a5cb7b2510d64db02b44ad7f68edeeb848
unknown <houarizegai14@gmail.com>
2019-06-04 21:15:41 +0100

    Making internationalization (multi lang app) using properties file

---
+++ src/com/houarizegai/javafxtools/internationalization/MultiLangController.java
@@ -0,0 +1,56 @@
+package com.houarizegai.javafxtools.internationalization;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MultiLangController implements Initializable {
+
+    @FXML
+    private Label lblUsername, lblPassword;
+
+    ResourceBundle bundle;
+    Locale locale;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void setLangAR() {
+        loadLang("ar");
+    }
+
+    @FXML
+    private void setLangEN() {
+        loadLang("en");
+    }
+
+    @FXML
+    private void setLangFR() {
+        loadLang("fr");
+    }
+
+    private void loadLang(String lang) {
+        locale = new Locale(lang);
+        bundle = ResourceBundle.getBundle("com.houarizegai.javafxtools.internationalization.lang", locale);
+
+        lblUsername.setText(toUTF(bundle.getString("username")));
+        lblPassword.setText(toUTF(bundle.getString("password")));
+    }
+
+    private String toUTF(String val) {
+        try {
+            return new String(val.getBytes("ISO-8859-1"), "UTF-8");
+        } catch(Exception ex) {
+
+        }
+        return null;
+    }
+
+}
