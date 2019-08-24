e45ce965d34e4d8d2080a879c7010eddf6f47cde
HouariZegai <houarizegai14@gmail.com>
2019-08-24 21:24:25 +0100

    Table - model 1 - adding action element in table (buttons, icons, ...)

---
+++ src/com/houarizegai/fxtools/tables/model1/Employee.java
@@ -0,0 +1,98 @@
+package com.houarizegai.fxtools.tables.model1;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import org.controlsfx.glyphfont.FontAwesome;
+
+public class Employee {
+    private JFXCheckBox selectCheck;
+    private int id;
+    private String name;
+    private Label statusLbl;
+    private HBox actionContainer;
+    private FontAwesomeIconView deleteIcon, editIcon;
+
+    private TableView containerTable;
+
+    public Employee(TableView containerTable, int id, String name, boolean isSuccess) {
+        this.id = id;
+        this.name = name;
+        this.containerTable = containerTable;
+
+        selectCheck = new JFXCheckBox();
+        selectCheck.getStyleClass().add("check-select");
+
+        initActions();
+        setStatus(isSuccess);
+    }
+
+    public JFXCheckBox getSelectCheck() {
+        return selectCheck;
+    }
+
+    public void setSelectCheck(JFXCheckBox selectCheck) {
+        this.selectCheck = selectCheck;
+    }
+
+    private void setStatus(boolean isSuccess) {
+        statusLbl = new Label();
+        statusLbl.getStyleClass().add("lbl");
+        if(isSuccess) {
+            statusLbl.setText("Success");
+            statusLbl.getStyleClass().add("lbl-success");
+        } else {
+            statusLbl.setText("Failed");
+            statusLbl.getStyleClass().add("lbl-failed");
+        }
+    }
+
+    private void initActions() {
+        deleteIcon = new FontAwesomeIconView(FontAwesomeIcon.TRASH);
+        deleteIcon.getStyleClass().addAll("icon-action", "icon-action-trash");
+        deleteIcon.setOnMouseClicked(e -> {
+            System.out.println(id + " - was clicked!");
+            containerTable.getItems().remove(this);
+        });
+
+        editIcon = new FontAwesomeIconView(FontAwesomeIcon.EDIT);
+        editIcon.getStyleClass().addAll("icon-action", "icon-action-edit");
+        editIcon.setOnMouseClicked(e -> {
+            System.out.println(id + " - edit icon was clicked!");
+        });
+
+        actionContainer = new HBox(20);
+        actionContainer.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        actionContainer.getChildren().addAll(editIcon, deleteIcon);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Label getStatusLbl() {
+        return statusLbl;
+    }
+
+    public HBox getActionContainer() {
+        return actionContainer;
+    }
+}
---
+++ src/com/houarizegai/fxtools/tables/model1/TableController.java
@@ -0,0 +1,51 @@
+package com.houarizegai.fxtools.tables.model1;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableController implements Initializable {
+    @FXML
+    private TableView<Employee> table;
+
+    @FXML
+    private TableColumn<Employee, ?> colSelect, colId, colName, colStatus, colActions;
+
+    public static TableView<Employee> staticTable;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        staticTable = table;
+        initTable();
+        loadTable();
+    }
+
+    private void initTable() {
+        for(TableColumn column: table.getColumns())
+            column.setStyle("-fx-alignment: center");
+
+        colSelect.setCellValueFactory(new PropertyValueFactory<>("selectCheck"));
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("statusLbl"));
+        colActions.setCellValueFactory(new PropertyValueFactory<>("actionContainer"));
+    }
+
+    private void loadTable() {
+        ObservableList<Employee> employees = FXCollections.observableArrayList();
+
+        employees.add(new Employee(table, 1, "Houari", true));
+        employees.add(new Employee(table, 2, "Moh", false));
+        employees.add(new Employee(table, 3, "Fatima", true));
+
+        table.getItems().addAll(employees);
+    }
+
+}
